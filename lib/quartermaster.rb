61793ec52caa096f63217d892dc617f880bfd51e
GoldenPavilion <rcorystine@gmail.com>
2021-02-02 13:03:15 -0500

    creating git

---
+++ lib/quartermaster.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "quartermaster/version"
+
+module Quartermaster
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/quartermaster/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Quartermaster
+  VERSION = "0.1.0"
+end
