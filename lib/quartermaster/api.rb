5f4400b5a0c1748f4521ee6405ba2ef8c8fe9ee1
GoldenPavilion <rcorystine@gmail.com>
2021-02-04 18:20:40 -0500

    set up get_item method

--- lib/quartermaster/api.rb
+++ lib/quartermaster/api.rb
@@ -1,5 +1,9 @@
 class Quartermaster::API
 
+    def get_item
+        items = RestClient.get('https://www.dnd5eapi.co/api/magic-items')
+        @items = JSON.parse(items)
+        binding.pry
+    end
 
-    
 end
