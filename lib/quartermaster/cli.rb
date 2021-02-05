de3b433d479a17546ea30f6392dfd6ce3f83a326
GoldenPavilion <rcorystine@gmail.com>
2021-02-04 22:42:22 -0500

    notated todo in call

--- lib/quartermaster/cli.rb
+++ lib/quartermaster/cli.rb
@@ -4,12 +4,19 @@ class Quartermaster::CLI
         greeting
         Quartermaster::API.get_items
         binding.pry
+        #present user with equipment categories
+        #get user input - based on index number they select
+        #present user with a list of items of the categories they selected
+            #get items from API
+            #iterate over items and assign their stats to a new object
+            #put out the item stats and details in a presentable way
+        #ask user whether they would like to search for another item
+
     end
 
     def greeting
         puts "Greetings morningrider."
         puts "I am Severn Desh, quartermaster of all things arcane."
-        puts "Would you like to see my inventory?"
-        puts "Careful what you pick. Not everything is as it seems."
+        puts "What exactly are you looking for?"
     end
 end
--- lib/quartermaster/magic-item.rb
+++ lib/quartermaster/magic-item.rb
@@ -1,5 +1,23 @@
 class MagicItem
 
+    attr_accessor :name, :index, :equip_category, :desc 
 
-    
+    @@all = []
+
+    def initialize(name, index, equip_category, desc)
+        @name = name 
+        @index = index
+        @equip_category = equip_category
+        @desc = desc
+        save
+    end 
+
+    def save 
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
 end
